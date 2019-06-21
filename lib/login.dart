592bffb7510433a4e0608517dd90e082d042b5b8
haminhkien1998 <minhkien2208@gmail.com>
2019-06-21 10:20:14 +0700

    Connect to server, create user, get massage

--- lib/login.dart
+++ lib/login.dart
@@ -1,8 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:toast/toast.dart';
 
 TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key}) : super(key: key);
+
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController usernameTxtCtrl = new TextEditingController();
+  final TextEditingController passwordTxtCtrl = new TextEditingController();
+  final TextEditingController reenterpasswordTxtCtrl =
+      new TextEditingController();
+  final TextEditingController emailTxtCtrl = new TextEditingController();
+  bool errUser = false, errPass = false, errRePass = false, errEmail = false;
+  String errTxt = "";
+
+  void sendData() async {
+    final uri = "http://142.93.253.93:81/web-api/new";
+    final headers = {'Content-Type': 'application/json'};
+    setState(() {
+      usernameTxtCtrl.text.isEmpty ? errUser = true : errUser = false;
+      emailTxtCtrl.text.isEmpty ? errEmail = true : errEmail = false;
+      passwordTxtCtrl.text.isEmpty ? errPass = true : errPass = false;
+      (!errPass && passwordTxtCtrl.text != reenterpasswordTxtCtrl.text)
+          ? errRePass = true
+          : errRePass = false;
+    });
+
+    if (passwordTxtCtrl.text != reenterpasswordTxtCtrl.text) {
+    } else {
+      Map<String, dynamic> body = {
+        'username': usernameTxtCtrl.text.trim(),
+        'password': passwordTxtCtrl.text,
+        'email': emailTxtCtrl.text.trim()
+      };
+      String jsonString = json.encode(body);
+      final encoding = Encoding.getByName('utf-8');
+      Response response = await post(uri,
+          headers: headers, body: jsonString, encoding: encoding);
+      String responseBody = response.body;
+      debugPrint(responseBody);
+      dynamic data = await json.decode(response.body);
+      if (data["statusCode"] == 0) {
+        setState(() {
+          errTxt = data["message"].toString();
+        });       
+      }
+
+      // usernameTxtCtrl.text = "";
+      // passwordTxtCtrl.text = "";
+      // reenterpasswordTxtCtrl.text = "";
+      // emailTxtCtrl.text = "";
+    }
+  }
+
+  void presss() async {
+    this.sendData();
+  }
+
   @override
   Widget build(BuildContext context) {
     final title = new Container(
@@ -13,30 +74,47 @@ class LoginPage extends StatelessWidget {
           )),
     );
 
+    final errNoti = new Container(
+      child: new Text(
+        errTxt,
+        style: TextStyle(fontSize: 20, color: Colors.red),
+      ),
+    );
+
     final usernameTextField = new Container(
       margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
-      child: new Theme(
-        data: new ThemeData(),
-        child: TextFormField(
-          autofocus: true,
-          decoration: InputDecoration(
+      child: TextField(
+        controller: usernameTxtCtrl,
+        onTap: () {
+          setState(() {
+            errUser = false;
+          });
+        },
+        decoration: InputDecoration(
             labelText: 'User name',
-            border: new OutlineInputBorder(borderSide: new BorderSide()),
-          ),
-          style: TextStyle(
-            fontSize: 20,
-          ),
+            border: new OutlineInputBorder(
+                borderSide: new BorderSide(color: Colors.blue)),
+            errorText: errUser ? "Please enter username" : null),
+        style: TextStyle(
+          fontSize: 20,
         ),
       ),
     );
 
     final passwordTextField = new Container(
       margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
-      child: TextFormField(
+      child: TextField(
         obscureText: true,
+        controller: passwordTxtCtrl,
+        onTap: () {
+          setState(() {
+            errPass = false;
+          });
+        },
         decoration: InputDecoration(
             labelText: 'Password',
-            border: new OutlineInputBorder(borderSide: new BorderSide())),
+            border: new OutlineInputBorder(borderSide: new BorderSide()),
+            errorText: errPass ? "Please enter password" : null),
         style: TextStyle(
           fontSize: 20,
         ),
@@ -45,11 +123,18 @@ class LoginPage extends StatelessWidget {
 
     final reenterPasswordTextField = new Container(
       margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
-      child: TextFormField(
+      child: TextField(
         obscureText: true,
+        controller: reenterpasswordTxtCtrl,
+        onTap: () {
+          setState(() {
+            errRePass = false;
+          });
+        },
         decoration: InputDecoration(
             labelText: 'Re-enter password',
-            border: new OutlineInputBorder(borderSide: new BorderSide())),
+            border: new OutlineInputBorder(borderSide: new BorderSide()),
+            errorText: errRePass ? "Password does not match" : null),
         style: TextStyle(
           fontSize: 20,
         ),
@@ -58,10 +143,17 @@ class LoginPage extends StatelessWidget {
 
     final emailTextField = new Container(
       margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
-      child: TextFormField(
+      child: TextField(
+        controller: emailTxtCtrl,
+        onTap: () {
+          setState(() {
+            errEmail = false;
+          });
+        },
         decoration: InputDecoration(
             labelText: 'Email',
-            border: new OutlineInputBorder(borderSide: new BorderSide())),
+            border: new OutlineInputBorder(borderSide: new BorderSide()),
+            errorText: errEmail ? "Please enter email" : null),
         style: TextStyle(
           fontSize: 20,
         ),
@@ -75,13 +167,17 @@ class LoginPage extends StatelessWidget {
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
+        onPressed: () {
+          // this.sendData();
+          this.presss();
+        },
         child: Text("Sign up",
             textAlign: TextAlign.center,
             style: style.copyWith(
                 color: Colors.white, fontWeight: FontWeight.bold)),
       ),
     );
+
     return new Scaffold(
       body: Container(
         padding: EdgeInsets.all(25.0),
@@ -102,13 +198,14 @@ class LoginPage extends StatelessWidget {
                 emailTextField
               ],
             ),
+            errNoti,
             Container(
               alignment: Alignment(0, 0.7),
               child: loginButon,
-            )
+            ),
           ],
         ),
       ),
-    );  
+    );
   }
 }
